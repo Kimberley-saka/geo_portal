2a637089704f79123eb4ed65eb382ac2328ef91c
Kimberley-saka <kimberleysaka@gmail.com>
2023-12-29 12:30:57 +0200

    Add Leaflet library and integrate map functionality

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -1,12 +1,25 @@
-export default function ExplorePage() {
+import './explore-styles/explore.css';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { useEffect, useRef } from 'react';
 
-  const getDatasets = () => {
-    
-  }
+export default function ExplorePage() {
+  const mapRef = useRef(null)
+  useEffect(() => {
+    if (!mapRef.current) { // Only create the map if it doesn't already exist
+      mapRef.current = L.map('map').setView([-13.254308, 34.301525], 7);
+  
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(mapRef.current);
+    }
+  }, []);
+
   return(
     <div className="flex flex-row px-8 md:px-24 md:my-14">
-      <div className="flex flex-col space-y-8 pt-4 md:w-1/3 md:h-screen">
-        <form className="w-full px-8 " onSubmit={getDatasets}>
+      <div className="flex flex-col border-transparent shadow-2xl shadow-sky-950 space-y-8 pt-4 
+      md:w-1/3 md:h-screen">
+        <form className="w-full px-8 pt-4">
           <div className="relative">
             <input className="flex-grow w-full h-9 rounded-full pl-5 pr-12" placeholder="Search datasets" />
             <button className="absolute top-0 right-0 mt-2 mr-2 pr-3
@@ -20,13 +33,12 @@ export default function ExplorePage() {
             </button>
           </div>
         </form>
-        <div className="pt-8 border-t-2 border-sky-950 bg-white h-full ">
+        <div id="datasets" className="pt-8 border-t-2 px-4 overflow-scroll border-sky-950 bg-white h-full ">
           datasets goes here
         </div>
       </div>
 
-      <div id="map" className="bg-sky-300 md:w-3/4">
-        Map goes here
+      <div id="map" className="h-screen w-full">
 
       </div>
   

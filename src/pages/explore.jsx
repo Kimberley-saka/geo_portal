39c5ce24d4ae0b4a11b4f8ae74fab3cf5f42f1ff
Kimberley-saka <kimberleysaka@gmail.com>
2023-12-29 11:26:01 +0200

    Update explore page form styling

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -5,14 +5,13 @@ export default function ExplorePage() {
   }
   return(
     <div className="flex flex-row px-8 md:px-24 md:my-14">
-      <div className="flex md:w-1/3 md:h-screen bg-yellow-200">
-        <form className="space-x-6 pt-6 px-6" onSubmit={getDatasets}>
+      <div className="flex justify-center pt-4 md:w-1/3 md:h-screen bg-yellow-200">
+        <form className="flex justify-center items-center w-full h-20 bg-pink-200 space-x-6 pt-6 px-6" onSubmit={getDatasets}>
           <input className="w-full h-9 rounded-full placeholder:pl-2" placeholder="Search datasets"></input>
           <button className="" type="submit">Go</button>
         </form>
 
-        <div>
-
+        <div className="">
         </div>
       </div>
 

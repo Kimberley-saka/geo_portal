e4b99ad82816d287879bde35ba843af1d59df406
Kimberley-saka <kimberleysaka@gmail.com>
2023-12-29 11:20:13 +0200

    Refactor explore page layout and add search functionality

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -4,15 +4,20 @@ export default function ExplorePage() {
     
   }
   return(
-    <div className="flex flex-row">
-      <div className="flex">
-        <form onSubmit={getDatasets}>
-          <input placeholder="Search datasets"></input>
-          <button type="submit">Go</button>
+    <div className="flex flex-row px-8 md:px-24 md:my-14">
+      <div className="flex md:w-1/3 md:h-screen bg-yellow-200">
+        <form className="space-x-6 pt-6 px-6" onSubmit={getDatasets}>
+          <input className="w-full h-9 rounded-full placeholder:pl-2" placeholder="Search datasets"></input>
+          <button className="" type="submit">Go</button>
         </form>
+
+        <div>
+
+        </div>
       </div>
 
-      <div id="map">
+      <div id="map" className="bg-sky-300">
+        Map goes here
 
       </div>
   
